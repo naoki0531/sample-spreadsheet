dfeea23a721b18494eb2189fecf22ead23927186
naoki <andvari100@gmail.com>
2018-02-05 19:55:06 +0900

    first commit

---
+++ src/components/SampleTable.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="example-container" class="wrapper">
+        <HotTable :root="root" :settings="hotSettings"></HotTable>
+    </div>
+</template>
+
+<script>
+  import HotTable from 'vue-handsontable-official';
+  import Vue from 'vue';
+
+  export default {
+    data: function () {
+      return {
+        root: 'test-hot',
+        hotSettings: {
+          colHeaders: ['name', 'price'],
+          rowHeaders: true,
+          data: this.data
+        },
+      }
+    },
+    name: 'SampleApp',
+    components: {
+      HotTable
+    },
+    props: ['data'],
+  }
+</script>
+
+<style>
+    #test-hot {
+        width: 600px;
+        height: 200px;
+        overflow: hidden;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import $ from 'jquery'
+
+// 追加
+import SampleTable from './components/SampleTable.vue'
+Vue.component('sample-table', SampleTable);
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  data: {
+    tableData: null,
+    show: false
+  },
+  created() {
+    setTimeout(() => {
+      this.tableData = [{"name":"apple","price":200},{"name":"orange","price":120}];
+      this.show = true;
+    }, 1000);
+  },
+  methods: {
+    register(data) {
+      alert(JSON.stringify(this.tableData) + '登録しました！');
+    }
+  },
+});
