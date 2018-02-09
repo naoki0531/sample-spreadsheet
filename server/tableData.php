17b55fa980820d41f837d1536211dc684009f06c
丸山　直樹 <uu106892@CA1750.local>
2018-02-09 12:47:28 +0900

    php5.3以下対応

--- server/tableData.php
+++ server/tableData.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $file = new SplFileObject('tableData.json', 'w');
-    $file->fwrite($_POST['data']);
+    $file->fwrite(stripslashes($_POST['data']));
 } else {
     $file = new SplFileObject('tableData.json');
     echo $file->fgets();
